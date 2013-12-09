1f73dab4071932889db7aaf61359542d3dcb375a
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2013-12-09 13:30:43 +0900

    fix

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 
-#include "libtest.h"
+
+#include "stdio.h"
+#include "string.h"
+
+void test() {
+	int *p = 0;
+ 
+    *p = 100; // Segmentation fault
+  
+   printf("%d\n", *p);
+}
 
 int main(void) {
 	test();
