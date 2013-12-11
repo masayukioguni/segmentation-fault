67f5315cf2808fba3a185174d72b3a8159c7b834
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2013-12-11 19:47:05 +0900

    fix

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
-
-
 #include "stdio.h"
 #include "string.h"
 
-void test() {
-	int *p = 0;
- 
+void test()
+{
+    int *p = 0;
+
     *p = 100; // Segmentation fault
-  
-   printf("%d\n", *p);
+
+    printf("%d\n", *p);
 }
 
-int main(void) {
-	test();
+int main(void)
+{
+    test();
 }
